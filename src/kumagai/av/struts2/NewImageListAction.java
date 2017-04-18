bfe6bb8e5f63f99bea73fb65f3b1f5baf76763c7
kumagai <iariro@gol.com>
2017-04-18 21:40:53 +0900

    new

---
+++ src/kumagai/av/struts2/NewImageListAction.java
@@ -0,0 +1,57 @@
+package kumagai.av.struts2;
+
+import java.util.*;
+import javax.servlet.*;
+import org.apache.struts2.*;
+import org.apache.struts2.convention.annotation.*;
+import kumagai.av.*;
+
+/**
+ * 画像リスト表示アクション用フォーム。
+ * @author kumagai
+ */
+@Namespace("/av")
+@Results
+({
+	@Result(name="success", location="/av/newimagelist.jsp"),
+	@Result(name="error", location="/av/error.jsp")
+})
+public class NewImageListAction
+{
+	public ArrayList<FileAndDatetime> newImageFilst;
+	public String message;
+
+	/**
+	 * 画像リスト表示アクション用フォーム。
+	 * @return 処理結果
+	 */
+	@Action("newimagelist")
+	public String execute()
+		throws Exception
+	{
+		ServletContext context = ServletActionContext.getServletContext();
+		String filePath = context.getInitParameter("AVImageFolder");
+
+		if (filePath != null)
+		{
+			try
+			{
+				newImageFilst = ImageCollection.getNewImageFilst(filePath);
+
+				return "success";
+			}
+			catch (Exception exception)
+			{
+				this.message = exception.getMessage();
+
+				return "error";
+			}
+		}
+		else
+		{
+			message = "必要なパラメータ定義が揃っていません";
+
+			return "error";
+		}
+	}
+}
