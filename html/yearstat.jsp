bec350b3a0108d9bff23f20317fb904b76240c65
kumagai <iariro@gol.com>
2021-10-31 18:47:31 +0900

    年ごと統計をHighchartsグラフ化 #1

--- html/yearstat.jsp
+++ html/yearstat.jsp
@@ -5,41 +5,30 @@
 <html>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-		<title>AV</title>
+		<meta http-equiv=Content-Style-Type content=text/css>
 		<link rel="stylesheet" type="text/css" href="hatena.css">
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+		<script type="text/javascript" src="http://code.highcharts.com/highcharts.js"></script>
+		<title>AV</title>
 	</head>
 
 	<body>
-
-		<h1>年ごと統計</h1>
-
-		<div class=hatena-body>
-		<div class=main>
-		<div class=day>
-
-		<table>
-			<tr>
-				<th>年</th>
-				<th width="80">年毎<br>レンタル数</th>
-				<th width="80">年毎<br>購入数</th>
-				<th width="80">リリース年毎<br>レンタル数</th>
-				<th width="80">リリース年毎<br>購入数</th>
-			</tr>
-
-			<s:iterator value="yearStat">
-				<tr>
-					<td><s:property value="year" />年</td>
-					<td align="right"><s:property value="rental2" />本</td>
-					<td align="right"><s:property value="buy2" />本</td>
-					<td align="right"><s:property value="rental1" />本</td>
-					<td align="right"><s:property value="buy1" />本</td>
-				</tr>
-			</s:iterator>
-		</table>
-
-		</div>
-		</div>
-		</div>
+		<div id="chart" style="width:1300px; height:700px"></div>
+		<script type="text/javascript">
+		function draw()
+		{
+			options =
+			{
+				chart: {type: 'column', renderTo: 'chart', plotBackgroundColor: 'lightgray'},
+				title: {text: '年ごと統計'},
+				xAxis: {categories: <s:property value="years" escapeHtml="false" /> },
+				yAxis: {title: '作品数'},
+				series: <s:property value="series" escapeHtml="false" />
+			};
+			chart = new Highcharts.Chart(options);
+		};
+		document.body.onload = draw();
+		</script>
 
 	</body>
 </html>
--- src/kumagai/av/struts2/YearStatAction.java
+++ src/kumagai/av/struts2/YearStatAction.java
@@ -1,12 +1,27 @@
 package kumagai.av.struts2;
 
-import java.sql.*;
-import java.util.*;
-import javax.servlet.*;
-import com.microsoft.sqlserver.jdbc.*;
-import org.apache.struts2.*;
-import org.apache.struts2.convention.annotation.*;
-import kumagai.av.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.ArrayList;
+import java.util.Collections;
+
+import javax.servlet.ServletContext;
+
+import org.apache.struts2.ServletActionContext;
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Namespace;
+import org.apache.struts2.convention.annotation.Result;
+import org.apache.struts2.convention.annotation.Results;
+
+import com.microsoft.sqlserver.jdbc.SQLServerDriver;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+
+import kumagai.av.ReleaseYearAndCountCollection;
+import kumagai.av.WatchYearAndCountCollection;
+import kumagai.av.YearAndCount;
+import kumagai.av.YearStat;
+import kumagai.av.YearStatRecord;
+import net.arnx.jsonic.JSON;
 
 /**
  * 年ごと統計表示アクション。
@@ -22,6 +37,26 @@ public class YearStatAction
 {
 	public YearStat yearStat;
 	public Exception exception;
+	public String years;
+	public String series;
+
+	/**
+	 * Highchartsのseries JSON用の型
+	 */
+	class NameAndData
+	{
+		public String name;
+		public ArrayList<Integer> data = new ArrayList<Integer>();
+
+		/**
+		 * nameを割り当て
+		 * @param name 軸名
+		 */
+		public NameAndData(String name)
+		{
+			this.name = name;
+		}
+	}
 
 	/**
 	 * 年ごと統計表示アクション。
@@ -57,6 +92,25 @@ public class YearStatAction
 
 			yearStat =
 				new YearStat(rental1Stat, buy1Stat, rental2Stat, buy2Stat);
+			Collections.reverse(yearStat);
+
+			ArrayList<String> years = new ArrayList<String>();
+			NameAndData rental1 = new NameAndData("年毎 レンタル数");
+			NameAndData buy1 = new NameAndData("年毎 購入数");
+			NameAndData rental2 = new NameAndData("リリース年毎 レンタル数");
+			NameAndData buy2 = new NameAndData("リリース年毎 購入数");
+
+			for (YearStatRecord yearStatRecord : yearStat)
+			{
+				years.add(String.format("%d年", yearStatRecord.year));
+				rental1.data.add(yearStatRecord.rental1);
+				buy1.data.add(yearStatRecord.buy1);
+				rental2.data.add(yearStatRecord.rental2);
+				buy2.data.add(yearStatRecord.buy2);
+			}
+
+			this.years = JSON.encode(years);
+			this.series = JSON.encode(new NameAndData [] {rental2, buy2, rental1, buy1});
 
 			return "success";
 		}
